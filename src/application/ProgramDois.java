133c2f135e8a8ce5127a6583a66c93c48dec8253
paulolatta <paulogn.lata@gmail.com>
2023-12-06 17:25:57 -0300

    department findById

---
+++ src/application/ProgramDois.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class ProgramDois {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+		System.out.println("=== TEST 1: findById =======");
+		Department dep = departmentDao.findById(2);
+		System.out.println(dep);
+		
+		sc.close();
+	}
+}
